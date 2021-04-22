3dc8eeb963954f0b704cdaaa5056c00cf1a1556d
Егор <egornesvickij@gmail.com>
2021-04-22 18:11:00 +0500

    Fix bugs at UP-BUTTON

--- scripts/up-button.js
+++ scripts/up-button.js
@@ -5,20 +5,13 @@ console.log(screenHeight);
 
 window.addEventListener("scroll", function () {
   pageY = pageYOffset;
-  // console.log(pageY);
-
-  if (screenHeight === 830 || screenHeight === 900) {
-    // console.log(900);
+  
     if (pageY >= 1800 && pageY < 5700 ) {
       upBtn.classList.add("up-btton")
       upBtn.classList.remove("no-fix")
     } else if (pageY < 1800) {
       upBtn.classList.remove("up-btton")
-    } else if (pageY >= 5700) {
-      upBtn.classList.remove("up-btton")
-      upBtn.classList.add("no-fix")
     } 
-  }
 });
 
 function backToTop() {
